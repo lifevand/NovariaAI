253264d400e134661ab79e03546f4b59cb63346d
FarizLifevander <lifevandergamedev@gmail.com>
2025-06-20 22:47:40 +0700

    Update image.js
    Signed-off-by: FarizLifevander <lifevandergamedev@gmail.com>

--- api/image.js
+++ api/image.js
@@ -22,7 +22,7 @@ export default async function handler(req, res) {
     }
 
     // Menggunakan model FLUX.1-dev
-    const MODEL_ID = 'black-forest-labs/FLUX.1-schnell';
+    const MODEL_ID = 'stabilityai/stable-diffusion-2-1p';
     const API_URL = `https://api-inference.huggingface.co/models/${MODEL_ID}`;
 
     console.log(`[API /api/image] Request: "${prompt}", Model: ${MODEL_ID}`);
